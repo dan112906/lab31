82691419e2d514d333e75aa1cc2ae1e8dc12f45c
dan112906 <91857207+dan112906@users.noreply.github.com>
2021-11-23 19:46:25 -0600

    Update index.php

--- index.php
+++ index.php
@@ -1,15 +1,12 @@
 <?php
-$enlace = mysqli_connect("20.121.205.92","equipo3","equipo3","personas","3306");
+ $dbhost = "20.121.205.92";
+ $dbuser = "equipo3";
+ $dbpass = "equipo3";
+ $db = "personas";
+ $conn = new mysqli($dbhost, $dbuser, $dbpass,$db) or die("Connect failed: %s\n". $conn -> error);
+ 
+ echo "conexion exitosa";
+ 
+ $conn -> close();
 
-if (!$enlace) {
-    echo "Error: No se pudo conectar a MySQL." . PHP_EOL;
-    echo "errno de depuración: " . mysqli_connect_errno() . PHP_EOL;
-    echo "error de depuración: " . mysqli_connect_error() . PHP_EOL;
-    exit;
-}
-
-echo "Éxito: Se realizó una conexión apropiada a MySQL! La base de datos mi_bd es genial." . PHP_EOL;
-echo "Información del host: " . mysqli_get_host_info($enlace) . PHP_EOL;
-
-mysqli_close($enlace);
 ?>
