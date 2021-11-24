f60cb9ea12bd5ac9a63d696b428e7226a882caf3
dan112906 <91857207+dan112906@users.noreply.github.com>
2021-11-23 19:59:20 -0600

    Update index.php

--- index.php
+++ index.php
@@ -3,10 +3,13 @@
  $dbuser = "equipo3";
  $dbpass = "equipo3";
  $db = "personas";
- $conn = new mysqli($dbhost, $dbuser, $dbpass,$db) or die("Connect failed: %s\n". $conn -> error);
- 
- echo "conexion exitosa";
- 
- $conn -> close();
+
+// Create connection
+$conn = mysqli_connect($dbhost, $dbuser, $dbpass,$db);
+// Check connection
+if (!$conn) {
+   die("Connection failed: " . mysqli_connect_error());
+}
+echo "Connected successfully";
 
 ?>
